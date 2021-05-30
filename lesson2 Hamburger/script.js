d8e2ba21ac385fa004ac6d7ce037ea959631729e
Rodion Dudar <rodion.dudar.rd@gmail.com>
2021-05-30 21:04:54 +0300

    add homework lesson two

---
+++ lesson2 Hamburger/script.js
@@ -0,0 +1,63 @@
+class Hamburger {
+    constructor (size, stuffing) {
+        this.size = size;
+        this.stuffing = stuffing;
+    }
+
+    /**
+     * Метод добавляет добавку.
+     * @param {string} topping вид добавки
+     */
+    addTopping(topping) {
+        
+    }
+
+    /**
+     * Метод удаляет добавку.
+     * @param {string} topping вид добавки.
+     */
+    removeTopping(topping) {
+
+    }
+
+    /**
+     * Метод возвращает список добавок.
+     * @param {string} topping вид добавки.
+     */
+    getToppings(topping) {
+        
+    }
+
+    /**
+     * Метод возвращает значение размера гамбургера
+     */
+    getSize() {
+        
+    }
+    /**
+     * Метод возвращает информацию о начинке гамбургера.
+     */
+    getStuffing() {
+    
+    }
+
+
+    /**
+     * Метод возвращает информацию о цене гамбургера.
+     */
+    getCalculatePrice() {
+
+    }
+
+    /**
+     * Метод считает количество калорий в гамбургере.
+     */
+    calculateCalories() {
+
+    }
+}
+
+const hamburger = new Hamburger();
+hamburger.getSize();
+
+
